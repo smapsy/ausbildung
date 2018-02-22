47c9aad39073baa8763cabcbd2515fa0585bcc2e
Actionboundsupport <support@actionbound.de>
2018-02-22 18:04:15 +0100

    transform abfrage

--- assp/assp.js
+++ assp/assp.js
@@ -5,35 +5,53 @@ var path = require('path')
 
 function processFile(file) {
   var args = file
-  var endings = [".js", ".js.coffee", ".css", ".css.less", ".jst.eco"]
+  var endings = [".js", ".js.coffee", ".css", ".css.less", ".jst.eco", ".css.scss"]
   var pfad = path.dirname(args)
+  var newEnding = ""
   //console.log(pfad)
 
-//Datei kann mit oder ohne Endung eingegeben werden
+  //Datei kann mit oder ohne Endung eingegeben werden
 
   if (fs.existsSync(args)) {
-    var newEnding = ""
+    newEnding = ""
   } else {
 
     for (var i = 0; i < endings.length; i++) {
       if (fs.existsSync(args + endings[i])) {
-        var newEnding = endings[i]
+        newEnding = endings[i]
         break
       }
     }
   }
 
-//Inhalt der Datei Ausgeben, wenn sie existiert
+   var trueEnding = path.extname(args + newEnding)
+
+  //Inhalt der Datei Ausgeben, wenn sie existiert
 
   try {
     var datei = fs.readFileSync(args + newEnding, { encoding: "utf8" })
   }
-  catch(err) {
+  catch (err) {
     console.log(args + " nicht vorhanden")
     process.exit(-1)
   }
 
-//Mit RegEx nach require und Pfad suchen  
+  switch (trueEnding) {
+    case ".less":
+      datei = transformLess(datei, pfad)
+      break;
+    case ".coffee":
+      datei = transformCoffee(datei)
+      break;
+    case ".scss":
+      datei = transformScss(datei)
+      break;
+    case ".eco":
+      datei = transformEco(datei, pfad)
+      break;
+  }
+
+  //Mit RegEx nach require und Pfad suchen  
 
   var regex1 = /^\s?(?:#=|\/\/=|\*=).(require(?:(?:_self)|(?:_tree))?)(?: (.*))?$/gm
   var match
@@ -44,7 +62,7 @@ function processFile(file) {
 
 
   //Switch über die verschiedenen require Arten, und wenn es sich um require handelt, dann die Funktion mit Pfad + Datei und rückgabe and Dateiinhalt Vorhängen.
-//Rekursion sich selbst aufrufende Funktion; Datei 2 um mit exec nicht die Datei aufzurufen, die dann geändert wird
+  //Rekursion sich selbst aufrufende Funktion; Datei 2 um mit exec nicht die Datei aufzurufen, die dann geändert wird
 
   while ((match = regex1.exec(datei2)) !== null) {
     var require = match[0]
@@ -54,8 +72,8 @@ function processFile(file) {
     //Pfad zusammensetzen aus pfad und requireFile; falls requireFile nicht vorhanden wegen require_self, leeren string anfügen
     var pfadFolder = path.join(pfad, requireFile ? requireFile : "")
     //console.log(match1, match2)
-  
-    switch(requireType) {
+
+    switch (requireType) {
       case "require":
         reqBlock = reqBlock + processFile(pfadFolder) + "\n"
         break;
@@ -65,7 +83,7 @@ function processFile(file) {
         break;
       case "require_tree":
         reqBlock = reqBlock + processFolder(pfadFolder) + "\n"
-        break; 
+        break;
     }
 
   }
@@ -74,7 +92,7 @@ function processFile(file) {
   } else {
     datei = reqBlock + datei
   }
-  
+
   return datei
   //return reqBlock
 }
@@ -84,7 +102,7 @@ function processFolder(folder) {
   var folderBlock = ""
 
   for (var i = 0; i < tree.length; i++) {
-    var pfad = path.join(folder, tree[i]) 
+    var pfad = path.join(folder, tree[i])
 
     if (fs.lstatSync(pfad).isDirectory()) {
       folderBlock = folderBlock + processFolder(pfad) + "\n"
@@ -96,9 +114,25 @@ function processFolder(folder) {
   return folderBlock
 }
 
+function transformLess(file, pfad) {
+  return file + "\n" + "//LESS Datei mit Pfad " + pfad
+}
+
+function transformCoffee(file) {
+  return file + "\n" + "//COFFEE Datei"
+}
+
+function transformScss(file) {
+  return file + "\n" + "//SCSS Datei"
+}
+
+function transformEco(file, pfad) {
+  return file + "\n" + "//ECO Datei mit Pfad " + pfad + " und Dateiname "
+}
 
 var output = processFile(process.argv[2])
 //var output = processFolder(process.argv[2])
+//var output = transformLess(process.argv[2])
 
 console.log(output)
 
